5363de93ee7f9b368887e6b6b522ffbfb2067d33
Kirill <kyrimer2010@gmail.com>
2024-04-18 18:37:56 +0300

    spelling lol

--- src/components/UserRegistrationForm.tsx
+++ src/components/UserRegistrationForm.tsx
@@ -54,7 +54,7 @@ const UserRegistrationForm = () => {
       <input type="text" name="surname" required placeholder="Фамилия" />
       <input type="email" name="email" required placeholder="Email" />
       <input type="password" name="password" required placeholder="Пароль" />
-      <button style={{ color: 'black' }}>Зарегестрироваться</button>
+      <button style={{ color: 'black' }}>Зарегистрироваться</button>
       {state?.error && <p>{state.error}</p>}
     </form>
   );
